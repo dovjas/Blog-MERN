b9065c4a85141a64b3feaa61f8a107f3d7898d46
Dovydas J <dovydas.jaseliunas@gmail.com>
2023-02-22 20:11:42 +0200

    created - homepage html and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-     <div>test</div>
-    </div>
+    <main>
+      <header>
+        <a href="" className="logo">Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="article">
+        <div className="image">
+          <img src="https://pbs.twimg.com/profile_images/1619236295020277761/8Tx5fmuZ_400x400.jpg"></img>
+        </div>        
+        <div className="content">
+          <h2>My first post</h2>
+          <p className="info">
+            <span className="author">Dovyydas J</span>
+            <time>2023.01.01 11:30</time>
+          </p>
+          <p className="summary">As you read this message, please take a moment to pause and breathe. Notice the sensations in your body and the rhythm of your breath. </p>
+        </div>
+      </div> 
+      <div className="article">
+        <div className="image">
+          <img src="https://pbs.twimg.com/profile_images/1619236295020277761/8Tx5fmuZ_400x400.jpg"></img>
+        </div>        
+        <div className="content">
+          <h2>My first post</h2>
+          <p className="info">
+            <span className="author">Dovyydas J</span>
+            <time>2023.01.01 11:30</time>
+          </p>
+          <p className="summary">As you read this message, please take a moment to pause and breathe. Notice the sensations in your body and the rhythm of your breath. </p>
+        </div>
+      </div> 
+      <div className="article">
+        <div className="image">
+          <img src="https://pbs.twimg.com/profile_images/1619236295020277761/8Tx5fmuZ_400x400.jpg"></img>
+        </div>        
+        <div className="content">
+          <h2>My first post</h2>
+          <p className="info">
+            <span className="author">Dovyydas J</span>
+            <time>2023.01.01 11:30</time>
+          </p>
+          <p className="summary">As you read this message, please take a moment to pause and breathe. Notice the sensations in your body and the rhythm of your breath. </p>
+        </div>
+      </div> 
+    </main>
   );
 }
 
