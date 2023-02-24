d2e3b2a67223928757351fdbb591ffc6f783c003
Dovydas J <dovydas.jaseliunas@gmail.com>
2023-02-24 23:47:42 +0200

    created - Registration form fetch

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,10 +1,32 @@
+import {useState} from "react";
 
 const Register = () =>{
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const registration = async (e) =>{
+        e.preventDefault();
+        await fetch('http://localhost:4000/register',{
+            method:'POST',
+            body: JSON.stringify({username,password}),
+            header:{'Content-Type':'application/json'}
+        });
+    }
     return(
-        <form className="register">
+        <form className="register" onSubmit={registration}>
         <h1>Registration</h1>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" /> 
+        <input 
+            type="text" 
+            placeholder="username"
+            value={username}
+            onChange={ e => setUsername(e.target.value)}
+         />
+        <input 
+            type="password" 
+            placeholder="password" 
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+        /> 
         <button>Register</button>
     </form>
     )
