691fce9ca1ca3d744a35bdb9229c317b5f56a981
Dovydas J <dovydas.jaseliunas@gmail.com>
2023-02-25 21:38:48 +0200

    added - redirect after success login

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 
 const Login = () => {
     const [username,setUsername] = useState('');
     const [password,setPassword] = useState('');
+    const [redirect,setRedirect] = useState('');
 
     async function login(e) {
         e.preventDefault();
-        await fetch('http://localhost:4000/login',{
+        const response = await fetch('http://localhost:4000/login',{
             method:'POST',
             body:JSON.stringify({username,password}),
-            headers:{'Content-Type':'application/json'}
+            headers:{'Content-Type':'application/json'},
+            credentials:'include'
         });
+        if(response.ok){
+            setRedirect(true);
+        }else{
+            alert('Wrong credentials')
+        }
     };
+    if(redirect){
+        return <Navigate to={'/'} />
+    }
 
     return(
         <form className="register" onSubmit={login}>
