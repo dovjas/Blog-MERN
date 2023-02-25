30936814acc44b05e2c7294dbb4d352c598d70d2
Dovydas J <dovydas.jaseliunas@gmail.com>
2023-02-25 20:38:03 +0200

    created - login function

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
 
 const Login = () => {
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+
+    async function login(e) {
+        e.preventDefault();
+        await fetch('http://localhost:4000/login',{
+            method:'POST',
+            body:JSON.stringify({username,password}),
+            headers:{'Content-Type':'application/json'}
+        });
+    };
+
     return(
-        <form className="register">
+        <form className="register" onSubmit={login}>
             <h1>Login</h1>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" /> 
+            <input 
+            type="text" 
+            placeholder="username"
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}
+            />
+            <input 
+                type="password" 
+                placeholder="password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+            /> 
             <button>Login</button>
         </form>
     )
