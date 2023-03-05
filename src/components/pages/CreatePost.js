2b67c936f9e34257d53dba61f111abdd0a6766ad
Dovydas J <dovydas.jaseliunas@gmail.com>
2023-03-05 12:34:18 +0200

    created - create new post function

--- src/components/pages/CreatePost.js
+++ src/components/pages/CreatePost.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 const CreatePost = () => {
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [content, setContent] = useState('');
+  const [files, setFiles] = useState('');
+
+  const createPost = (e) =>{
+    e.preventDefault();
+    const data = new FormData();
+      data.set = ('title', title);
+      data.set = ('summary', summary);
+      data.set = ('content', content);
+      data.set = ('file', files[0]);
+
+      fetch('http://localhost:4000/create',{
+        method: 'POST',
+        body: data,
+      });
+  };
+
+
+
   return (
     <div>
-        <form>
-            <input type="title" placeholder={'Title'} />
-            <input type="summary" placeholder={'Summary'} />
-            <input type="file" />
-            <ReactQuill />
+        <form onSubmit={createPost}>
+            <input 
+              type="title" 
+              placeholder={'Title'}
+              value={title}
+              onChange={(e)=>setTitle(e.target.value)}                
+            />
+            <input 
+              type="summary" 
+              placeholder={'Summary'} 
+              value={summary}
+              onChange={(e)=>setSummary(e.target.value)}
+            />
+            <input type="file" onChange={(e)=>setFiles(e.target.files)} />
+            <ReactQuill
+              value={content}
+              onChange={newValue =>setContent(newValue)}
+            />
             <button style={{marginTop:'5px'}}>Create Now</button>
         </form>
 
